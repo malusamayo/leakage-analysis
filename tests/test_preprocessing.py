da95d4fec7c6acb4b5f3d90bbdb4dfc305194810
malusamayo <773386@gmail.com>
2022-01-18 15:53:09 -0500

    update entry to preprocessing tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -2,14 +2,14 @@ import os, sys
 import pandas as pd
 from src import main
 
-leak_method_list = ["LogisticRegression.fit", "trainSinkPoint"]
+leak_method_list = ["LogisticRegression.fit", "GaussianNB.fit", "Unknown.fit"]
 
 def template(test_file, shouldBeTainted):
     test_file_path = os.path.join(".", "tests", "inputs", test_file)
     test_fact_path = os.path.join("tests", "inputs", test_file.replace(".py", "-fact"), "Leak.csv")
     main.main(test_file_path)
     assert os.path.exists(test_fact_path)
-    df = pd.read_csv(test_fact_path, sep="\t", names=["heap", "invo", "method"])
+    df = pd.read_csv(test_fact_path, sep="\t", names=["heap", "method"])
     isTainted = df["method"].map(lambda m: m in leak_method_list).any()
     assert isTainted == shouldBeTainted, "Leak undetected!" if shouldBeTainted else "False leak detected!"
 
@@ -28,3 +28,10 @@ def test_titanic():
 
 def test_tfidf():
     template("nb_100841.py", shouldBeTainted=True)
+
+def test_dataFrameMapper():
+    template("nb_132929.py", shouldBeTainted=True)
+
+def test_scaler():
+    template("nb_175471.py", shouldBeTainted=True)
+    
