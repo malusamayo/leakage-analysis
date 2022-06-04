18e0becb1cf5784a6ca20c502b33acafd8adcbcb
malusamayo <773386@gmail.com>
2022-06-04 13:55:42 -0400

    update pyright path

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ class Config(object):
     def __init__(self, inference_path: str, output_flag: bool) -> None:
         self.inference_path = inference_path
         self.output_flag = output_flag
-configs = Config("../pyright/packages/pyright/index.js", True)
+configs = Config("./pyright/packages/pyright/index.js", True)
